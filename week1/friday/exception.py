cfda5ecca3d6ae32a55f1e3ace40503f19053f66
Kelvin Moi <kelvin.mai002@gmail.com>
2017-11-17 11:53:22 -0600

    updated exceptions file

--- week1/friday/exception.py
+++ week1/friday/exception.py
@@ -14,26 +14,21 @@ while True:
             print("Please enter valid value.")
             break
 
-        try:
-            operand = raw_input("Enter operand: ")
-        except ValueError:
-            print("Please enter valid value.")
-            break
+        operand = raw_input("Enter operand: ")
+        if operand == "+":
+            print(calc.add(num1,num2))
+        elif operand == "-":
+            print(calc.subtract(num1,num2))
+        elif operand == "*":
+            print(calc.multiply(num1,num2))
+        elif operand == "/":
+            try:
+                print(calc.divide(num1,num2))
+            except ZeroDivisionError:
+                print("Cannot divide by zero.")
+        else:
+            print("Invalid operand.")
         break
-
-    if operand == "+":
-        print(calc.add(num1,num2))
-    elif operand == "-":
-        print(calc.subtract(num1,num2))
-    elif operand == "*":
-        print(calc.multiply(num1,num2))
-    elif operand == "/":
-        try:
-            print(calc.divide(num1,num2))
-        except ZeroDivisionError:
-            print("Cannot divide by zero.")
-    else:
-        print("Invalid operand.")
 
     if raw_input("Press any key to continue or q to quit: ").lower() == "q":
         break
